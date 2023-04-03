e457165ad8e537d1e7c633d28d3ad23f0fafe527
lejinvarghese <lejinsnests@gmail.com>
2023-04-02 20:33:58 -0400

    minimal
    Signed-off-by: lejinvarghese <lejinsnests@gmail.com>

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from transformers import GPT2Tokenizer
 load_dotenv()
 
 COMET_API_KEY = os.getenv("COMET_API_KEY")
+params = {
+    "model": "distilgpt2",
+    "epochs": 50,
+    "batch_size": 32,
+    "learning_rate": 2e-5,
+    "weight_decay": 0.01,
+}
+
 experiment = comet_ml.Experiment(
     api_key=COMET_API_KEY,
     project_name="clm",
@@ -27,15 +35,11 @@ def tokenize_function(examples):
 
 
 def group_texts(examples):
-    # Concatenate all texts.
     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
     total_length = len(concatenated_examples[list(examples.keys())[0]])
-    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-    # customize this part to your needs.
-    # block_size = tokenizer.model_max_length
     block_size = 128
     total_length = (total_length // block_size) * block_size
-    # Split by chunks of max_len.
+
     result = {
         k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
         for k, t in concatenated_examples.items()
@@ -50,17 +54,9 @@ from transformers import AutoConfig, TFAutoModelForCausalLM
 from transformers import AdamWeightDecay
 from transformers import DefaultDataCollator
 import math
-import pandas as pd
 
 
-if __name__ == "__main__":
-    params = {
-        "model": "distilgpt2",
-        "epochs": 50,
-        "batch_size": 32,
-        "learning_rate": 2e-5,
-        "weight_decay": 0.01,
-    }
+def train():
     experiment.log_parameters(params)
     model_checkpoint = params["model"]
     tokenizer_checkpoint = params["model"]
@@ -88,7 +84,7 @@ if __name__ == "__main__":
 
     ## tokenize
     tokenizer = GPT2Tokenizer.from_pretrained(params["model"])
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
+    # tokenizer = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
     tokenized_datasets = filtered_dataset.map(
         tokenize_function,
         batched=True,
@@ -114,14 +110,14 @@ if __name__ == "__main__":
 
     ## train
     config = AutoConfig.from_pretrained(model_checkpoint)
-    gpt2 = TFAutoModelForCausalLM.from_config(config)
+    model = TFAutoModelForCausalLM.from_config(config)
     learning_rate = params["learning_rate"]
     weight_decay = params["weight_decay"]
 
     optimizer = AdamWeightDecay(
         learning_rate=learning_rate, weight_decay_rate=weight_decay
     )
-    gpt2.compile(optimizer=optimizer)
+    model.compile(optimizer=optimizer)
 
     data_collator = DefaultDataCollator(return_tensors="tf")
 
@@ -132,7 +128,12 @@ if __name__ == "__main__":
         collate_fn=data_collator,
     )
 
-    # gpt2.fit(train_set, epochs=params["epochs"])
-    # eval_loss = gpt2.evaluate(train_set)
-    # experiment.log_metrics({"eval_loss": eval_loss})
-    # print(f"Perplexity: {math.exp(eval_loss):.2f}")
+    model.fit(train_set, epochs=params["epochs"])
+    train_loss = model.evaluate(train_set)
+    experiment.log_metrics({"train_loss": train_loss})
+    print(f"Perplexity: {math.exp(train_loss):.2f}")
+    model.save("trained_model")
+
+
+if __name__ == "__main__":
+    train()
