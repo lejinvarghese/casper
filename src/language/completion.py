f2f4aac1b015fa0eeb350754d80a1a35822e343b
lejinvarghese <lejinsnests@gmail.com>
2023-12-30 12:28:34 -0500

    split completion and chat engine
    Signed-off-by: lejinvarghese <lejinsnests@gmail.com>

--- src/language/completion.py
+++ src/language/completion.py
@@ -1,20 +1,27 @@
 from src.language.models import InstuctModel, EmbeddingModel
 from src.language.storage import Storage
 
-from src.language.utils.logger import StreamingLogger
+from src.language.utils.logger import CustomLogger, StreamingLogger
 
+logger = CustomLogger(__name__)
 st_logger = StreamingLogger(__name__)
 llm = InstuctModel()
 emb = EmbeddingModel(device="cuda")
 index = Storage(llm=llm.model, embed_model=emb.model).load_vector_index()
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+query_engine = index.as_query_engine(streaming=True)
 
 def main():
         while True:
             st_logger.debug("Enter your query: \n")
             user_query = input()
+            response = llm.generate(user_query, streaming=True)
+            logger.debug("Raw response:")
+            for r in response:
+                st_logger.debug(r.delta)
+            st_logger.flush()
             
-            response = chat_engine.stream_chat(user_query)
+            response = query_engine.query(user_query)
+            logger.info("Contextual response:")
             for r in response.response_gen:
                 st_logger.info(r)
             st_logger.flush()
