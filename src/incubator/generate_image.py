1fc0595050bd3cfc52a61ac0239a0974f1315622
lejin <lejinsnests@gmail.com>
2024-12-25 13:14:06 -0500

    add minor mods to image
    Signed-off-by: lejin <lejinsnests@gmail.com>

--- src/incubator/generate_image.py
+++ src/incubator/generate_image.py
@@ -1,13 +1,28 @@
+import click
 from diffusers import StableDiffusionPipeline
-import torch
+from torch import bfloat16
 
-# Load the pre-trained model
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16)
-pipe.to("cuda")
 
-# Generate image from text query
-query = "fantasy art, astronaut, in space"
-image = pipe(query).images[0]
+@click.command()
+@click.option(
+    "--model_id",
+    default="CompVis/stable-diffusion-v1-4",
+    help="Model ID to use for generating image",
+)
+@click.option(
+    "--prompt",
+    default="fantasy art, astronaut, in space",
+    help="Prompt to generate image",
+)
+@click.option("--device", default="cuda", help="Device to use for generating image")
+def generate_image(model_id, prompt, device):
+    pipeline = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=bfloat16)
+    pipeline.to(device)
+    image = pipeline(prompt).images[0]
+    file_path = f"src/data/.images/generated_image_{prompt.replace(' ', '_')}.png"
+    image.save(file_path)
+    return image
 
-# Save or display the image
-image.save("generated_image.png")
+
+if __name__ == "__main__":
+    generate_image()
