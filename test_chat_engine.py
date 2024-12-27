09c0fd476bf7250aa7d74603db2315a62ea611ad
lejin <lejinsnests@gmail.com>
2024-12-26 22:20:50 -0500

    dynamic tests passed
    Signed-off-by: lejin <lejinsnests@gmail.com>

--- test_chat_engine.py
+++ test_chat_engine.py
@@ -1,7 +1,23 @@
 import pytest
 from chat import ChatEngine
 from src.utils.logger import BaseLogger
+
 logger = BaseLogger(__name__)
+
+
+@pytest.fixture(scope="session")
+def chat_engine():
+    return ChatEngine()
+
+
+@pytest.fixture
+def dynamic_chat_engine(chat_engine, request):
+    """Dynamically configure the ChatEngine based on the test's needs."""
+    chat_mode = request.param.get("chat_mode")
+    persona = request.param.get("persona")
+    chat_engine.update_config(chat_mode=chat_mode, persona=persona)
+    return chat_engine
+
 
 @pytest.fixture
 def sample_rag_query():
@@ -12,27 +28,30 @@ def sample_rag_query():
 def sample_persona_query():
     return "Who are you?"
 
-@pytest.mark.xdist_group(name="llm")
-def test_simple_chat(sample_rag_query):
-    engine = ChatEngine()
-    response = str(engine.chat(sample_rag_query)).lower()
-    logger.info(response)
-    assert response is not None
-    assert "riga" not in response
 
 @pytest.mark.xdist_group(name="llm")
-def test_rag_chat(sample_rag_query):
-    engine = ChatEngine(chat_mode="condense_plus_context")
-    response = str(engine.chat(sample_rag_query)).lower()
+@pytest.mark.parametrize(
+    "dynamic_chat_engine",
+    [
+        {"chat_mode": "simple", "persona": "casper"},
+        {"chat_mode": "condense_plus_context"},
+        {"chat_mode": "simple", "persona": "cassia"},
+    ],
+    indirect=True,
+)
+def test_chat(dynamic_chat_engine, sample_rag_query, sample_persona_query):
+    query = (
+        sample_rag_query
+        if dynamic_chat_engine.chat_mode == "condense_plus_context"
+        else sample_persona_query
+    )
+    response = str(dynamic_chat_engine.chat(query)).lower()
     logger.info(response)
-    assert response is not None
-    assert "riga" in response
 
-@pytest.mark.xdist_group(name="llm")
-def test_persona_chat(sample_persona_query):
-    persona = "cassia"
-    engine = ChatEngine(persona=persona)
-    response = str(engine.chat(sample_persona_query)).lower()
-    logger.info(response)
-    assert response is not None
-    assert persona in response
+    # Assert based on configuration
+    if dynamic_chat_engine.chat_mode == "condense_plus_context":
+        assert "riga" in response
+    elif dynamic_chat_engine.persona == "cassia":
+        assert "cassia" in response
+    if dynamic_chat_engine.chat_mode == "simple":
+        assert "riga" not in response
